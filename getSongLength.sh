c304cdd7549ac49d0f3e0082e504ce9b1d9fd88f
wibus-wee <1596355173@qq.com>
2022-08-06 12:42:11 +0800

    修改「那有一天不想你」cover 链接

--- getSongLength.sh
+++ getSongLength.sh
@@ -2,5 +2,5 @@
 for file in `find . -name "*.mp3"`
 do
     echo $file
-    echo `ffmpeg -i $file 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,//`
+    echo `ffmpeg -i $file 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,// | awk -F: '{print ($1*3600+$2*60+$3)*100}'`
 done
